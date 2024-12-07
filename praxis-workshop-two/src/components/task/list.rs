db53c4883639a88f511b5e1d0307d02e12deecfa
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2024-12-07 12:26:02 -0800

    feat: get tasks and pass to list component

--- praxis-workshop-two/src/components/task/list.rs
+++ praxis-workshop-two/src/components/task/list.rs
@@ -1,10 +1,19 @@
 use leptos::*;
 
+use crate::api::tasks::Task;
+
 #[component]
-pub fn TasksList() -> impl IntoView {
+pub fn TasksList(tasks: Vec<Task>) -> impl IntoView {
+    println!("TasksList: {:?}", tasks);
     view! {
         <div>
             <h2>"Tasks List"</h2>
+            <ul>
+                {tasks
+                    .iter()
+                    .map(|task| view! { <li>{task.title.clone()}</li> })
+                    .collect_view()}
+            </ul>
         </div>
     }
 }
--- praxis-workshop-two/src/pages/tasks/list.rs
+++ praxis-workshop-two/src/pages/tasks/list.rs
@@ -1,4 +1,5 @@
 use leptos::*;
+use leptos_dom::logging::console_log;
 
 use crate::api::tasks::Task;
 use crate::components::task::form::TaskForm;
@@ -10,17 +11,29 @@ pub fn TasksListPage() -> impl IntoView {
     let (tasks, create, update, delete) = use_tasks();
     let on_add = move |title| {
         create.dispatch(Task {
-            id: 0, // or generate a unique ID if needed
+            id: 0,
             title,
             description: None,
             status: "pending".to_string(),
+            completed: false,
         })
     };
+
     view! {
         <>
             <h1>"Tasks Manager"</h1>
             <TaskForm on_add=on_add />
-            <TasksList />
+            {move || match tasks.get() {
+                None => view! { <div>"Loading..."</div> },
+                Some(Ok(tasks)) => {
+                    console_log(&format!("Tasks received: {:?}", tasks));
+                    view! { <div><TasksList tasks=tasks /></div> }
+                },
+                Some(Err(err)) => {
+                    console_log(&format!("Error details: {:?}", err));
+                    view! { <div>"Error loading tasks: " {err}</div> }
+                },
+            }}
         </>
     }
 }

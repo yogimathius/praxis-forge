6fd4a10e2d1fcf5d4f7f610c26e09bc7f5b6d392
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2024-12-07 11:07:38 -0800

    feat: add basic html to tasks list page

---
+++ praxis-workshop-two/src/pages/tasks/list.rs
@@ -0,0 +1,10 @@
+use leptos::*;
+
+#[component]
+pub fn TasksListPage() -> impl IntoView {
+    view! {
+        <div class="container">
+            <h1>"Tasks List Page"</h1>
+        </div>
+    }
+}

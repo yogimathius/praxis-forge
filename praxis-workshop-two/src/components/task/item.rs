a8105014579487ce07b9fd959d20e522ab41586e
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2024-12-07 11:52:12 -0800

    wip: add item and list component structure

---
+++ praxis-workshop-two/src/components/task/item.rs
@@ -0,0 +1,10 @@
+use leptos::*;
+
+#[component]
+pub fn TaskItem() -> impl IntoView {
+    view! {
+        <div>
+            <h3>"Task Item"</h3>
+        </div>
+    }
+}
---
+++ praxis-workshop-two/src/components/task/list.rs
@@ -0,0 +1,10 @@
+use leptos::*;
+
+#[component]
+pub fn TasksList() -> impl IntoView {
+    view! {
+        <div>
+            <h2>"Tasks List"</h2>
+        </div>
+    }
+}

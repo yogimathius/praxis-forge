675299daa3538a2a60d193a9f3e5f138d23dabeb
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2024-12-17 17:36:12 -0800

    wip: add live dashboard stuff

--- forge-core/lib/task_api_web/router.ex
+++ forge-core/lib/task_api_web/router.ex
@@ -5,25 +5,26 @@ defmodule TaskApiWeb.Router do
     plug :accepts, ["json"]
   end
 
+  # Browser pipeline for LiveDashboard
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   scope "/api", TaskApiWeb do
     pipe_through :api
-
     resources "/tasks", TaskController, except: [:new, :edit]
   end
 
-  # Enable LiveDashboard in development
-  if Application.compile_env(:task_api, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
+  # LiveDashboard route
+  if Mix.env() in [:dev] do
     import Phoenix.LiveDashboard.Router
 
-    scope "/dev" do
-      pipe_through [:fetch_session, :protect_from_forgery]
-
-      live_dashboard "/dashboard", metrics: TaskApiWeb.Telemetry
+    scope "/" do
+      pipe_through :browser
+      live_dashboard "/dashboard"
     end
   end
 end

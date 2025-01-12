0670d157d3a9d2c8c89227298b82fce804ffabea
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2025-01-11 22:59:59 -0800

    feat: improve app styles

--- praxis-workshop/src/app.rs
+++ praxis-workshop/src/app.rs
@@ -1,9 +1,11 @@
 use crate::pages::home::HomePage;
 use crate::pages::tasks::dashboard::TasksListPage;
 use leptos::*;
+use leptos_dom::logging::console_log;
 use leptos_router::*;
 use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+use web_sys::console;
 
 #[wasm_bindgen]
 extern "C" {
@@ -23,19 +25,43 @@ extern "C" {}
 pub fn App() -> impl IntoView {
     view! {
         <Router>
-            <main class="container">
-                <h1 class="title">"Praxis Workshop"</h1>
-                <nav class="nav">
-                    <A class="link" href="/">"Home"</A>
-                    <A class="link" href="/tasks">"Tasks"</A>
-                </nav>
-                <Routes>
-                    <Route path="/" view=HomePage/>
-                    <Route path="/tasks" view=TasksListPage/>
-                    <Route path="/*any" view=NotFound/>
-                </Routes>
-            </main>
+            <MainContent />
         </Router>
+    }
+}
+
+#[component]
+fn MainContent() -> impl IntoView {
+    let (location, set_location) = create_signal("/");
+    let message = format!("location: {:?}", location.get());
+
+    console::log_1(&JsValue::from_str(&message));
+
+    let location = use_location();
+
+    let current_location = move || location.pathname.get();
+
+    console_log(&format!("Current location: {}", current_location()));
+
+    let title = match current_location().as_str() {
+        "/" => "Home - Praxis Workshop",
+        "/tasks" => "Forge Operations - Praxis Workshop",
+        _ => "404 Not Found - Praxis Workshop",
+    };
+
+    view! {
+        <main class="container">
+            <h1 class="title">{title}</h1>
+            <nav class="nav">
+                <A class="link" href="/">"Home"</A>
+                <A class="link" href="/tasks">"Tasks"</A>
+            </nav>
+            <Routes>
+                <Route path="/" view=HomePage/>
+                <Route path="/tasks" view=TasksListPage/>
+                <Route path="/*any" view=NotFound/>
+            </Routes>
+        </main>
     }
 }
 

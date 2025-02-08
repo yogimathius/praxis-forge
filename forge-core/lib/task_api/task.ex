f5378e065f93ea1df7bc6183002545af31f24d68
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2025-02-08 11:16:04 -0800

    chore: add goal_id to task model

--- forge-core/lib/task_api/task.ex
+++ forge-core/lib/task_api/task.ex
@@ -6,18 +6,19 @@ defmodule TaskApi.Task do
     field :title, :string
     field :completed, :boolean, default: false
     field :description, :string
-    field :status, :string
-    belongs_to :goal, TaskApi.Goal  # Establish the relationship
+    field :status, :string, default: "pending"
+    belongs_to :goal, TaskApi.Goal
 
-    timestamps(type: :utc_datetime)
+    timestamps()
   end
 
   @doc false
   def changeset(task, attrs) do
     task
-    |> cast(attrs, [:title, :description, :status])
+    |> cast(attrs, [:title, :completed, :description, :status, :goal_id])
     |> validate_required([:title, :status])
     |> validate_length(:title, min: 3)
     |> validate_inclusion(:status, ["pending", "in_progress", "completed"])
+    |> foreign_key_constraint(:goal_id)
   end
 end

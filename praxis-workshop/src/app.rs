74cb03905c40b0043086ff5fe7aa9b87f7f3c618
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2025-02-15 23:29:00 -0800

    feat: provide graphql context in app

--- praxis-workshop/src/app.rs
+++ praxis-workshop/src/app.rs
@@ -1,15 +1,21 @@
 use leptos::*;
 use leptos_router::*;
+use std::sync::Arc;
 
 use crate::components::nav::Navigation;
 use crate::pages::goals::goals::GoalsListPage;
 use crate::pages::home::HomePage;
 use crate::pages::tasks::dashboard::TasksListPage;
+use crate::services::graphql_service::GraphQLService;
+use crate::services::service_context::ServiceContext;
 use crate::state::location::use_location;
 
 #[component]
-pub fn app() -> impl IntoView {
+pub fn App() -> impl IntoView {
     let location_state = use_location();
+    let service = GraphQLService::new();
+
+    provide_context(ServiceContext(Arc::new(service)));
 
     view! {
         <Router>

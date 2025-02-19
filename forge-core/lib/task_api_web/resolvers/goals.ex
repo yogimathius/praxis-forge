381a43ca6a237f3493e231852232a15359e3d502
Mathius Edwin Johnson <60864543+yogimathius@users.noreply.github.com>
2025-02-18 17:27:03 -0800

    chore: add logging for goals and tasks in elixir

--- forge-core/lib/task_api_web/resolvers/goals.ex
+++ forge-core/lib/task_api_web/resolvers/goals.ex
@@ -1,26 +1,52 @@
 defmodule TaskApiWeb.Resolvers.Goals do
+  require Logger
   alias TaskApi.Repo
   alias TaskApi.Goal
 
   def list_goals(_parent, _args, _resolution) do
+    Logger.info("Fetching all goals")
     {:ok, Repo.all(Goal)}
   end
 
   def create_goal(_parent, args, _resolution) do
     %Goal{}
     |> Goal.changeset(args)
-    |> Repo.insert()
+        |> Repo.insert()
+    |> case do
+      {:ok, goal} ->
+        Logger.info("Successfully created goal: #{inspect(goal)}")
+        {:ok, goal}
+      {:error, changeset} ->
+        Logger.error("Failed to create goal: #{inspect(changeset.errors)}")
+        {:error, changeset}
+    end
   end
 
   def update_goal(_parent, args, _resolution) do
     %Goal{}
     |> Goal.changeset(args)
-    |> Repo.update()
+    |> Repo.update( )
+    |> case do
+      {:ok, goal} ->
+        Logger.info("Successfully updated goal: #{inspect(goal)}")
+        {:ok, goal}
+      {:error, changeset} ->
+        Logger.error("Failed to update goal: #{inspect(changeset.errors)}")
+        {:error, changeset}
+    end
   end
 
   def delete_goal(_parent, args, _resolution) do
     %Goal{}
     |> Goal.changeset(args)
     |> Repo.delete()
+    |> case do
+      {:ok, goal} ->
+        Logger.info("Successfully deleted goal: #{inspect(goal)}")
+        {:ok, goal}
+      {:error, changeset} ->
+        Logger.error("Failed to delete goal: #{inspect(changeset.errors)}")
+        {:error, changeset}
+    end
   end
 end
--- forge-core/lib/task_api_web/resolvers/tasks.ex
+++ forge-core/lib/task_api_web/resolvers/tasks.ex
@@ -1,26 +1,62 @@
 defmodule TaskApiWeb.Resolvers.Tasks do
+  require Logger
   alias TaskApi.Repo
   alias TaskApi.Task
 
   def list_tasks(_parent, _args, _resolution) do
+    Logger.info("Fetching all tasks")
     {:ok, Repo.all(Task)}
   end
 
   def create_task(_parent, args, _resolution) do
-    %Task{}
+    Logger.info("Creating task with args: #{inspect(args)}")
+    result = %Task{}
     |> Task.changeset(args)
     |> Repo.insert()
+
+    case result do
+      {:ok, task} ->
+        Logger.info("Successfully created task: #{inspect(task)}")
+        result
+      {:error, changeset} ->
+        Logger.error("Failed to create task: #{inspect(changeset.errors)}")
+        result
+    end
   end
 
   def update_task(_parent, args, _resolution) do
-    %Task{}
+    Logger.info("Updating task with args: #{inspect(args)}")
+    result = %Task{}
     |> Task.changeset(args)
     |> Repo.update()
+
+    case result do
+      {:ok, task} ->
+        Logger.info("Successfully updated task: #{inspect(task)}")
+        result
+      {:error, changeset} ->
+        Logger.error("Failed to update task: #{inspect(changeset.errors)}")
+        result
+    end
   end
 
-  def delete_task(_parent, args, _resolution) do
-    %Task{}
-    |> Task.changeset(args)
-    |> Repo.delete()
+  def delete_task(_parent, %{id: id}, _resolution) do
+    Logger.info("Attempting to delete task with id: #{id}")
+
+    case Repo.get(Task, id) do
+      nil ->
+        {:error, "Task not found"}
+
+      task ->
+        case Repo.delete(task) do
+          {:ok, deleted_task} ->
+            Logger.info("Successfully deleted task with id: #{id}")
+            {:ok, deleted_task}
+
+          {:error, changeset} ->
+            Logger.error("Failed to delete task: #{inspect(changeset.errors)}")
+            {:error, "Failed to delete task"}
+        end
+    end
   end
 end

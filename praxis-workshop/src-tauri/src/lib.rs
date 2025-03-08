954c15b75f2337cc6470f6ceed6405bf605cdeed
Mathius Johnson <mathius.johnson@shopify.com>
2025-03-08 12:19:36 -0800

    feat: invoke config fns in tauri lib

--- praxis-workshop/src-tauri/src/lib.rs
+++ praxis-workshop/src-tauri/src/lib.rs
@@ -1,3 +1,6 @@
+mod config;
+use config::get_openai_api_key;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -8,7 +11,7 @@ fn greet(name: &str) -> String {
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![greet, get_openai_api_key])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
